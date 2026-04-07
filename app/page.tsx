21c4b65b319b61e19f802feb6d200cf78eb42f65
Nei -_- <VincentQu888@gmail.com>
2026-04-07 02:45:54 -0400

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <p>vincent qu's portfolio</p>
+      <p>this portfolio is gonna be fire trust</p>
+      <p>wip</p>
+    </>
+  );
+}
