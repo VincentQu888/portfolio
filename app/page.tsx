a70d7f0207502df7e9db11087415c80893e1f4c1
Nei -_- <VincentQu888@gmail.com>
2026-04-07 04:15:28 -0400

    added uoft webring

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="p-8">
-      <p><a href="https://github.com/VincentQu888" target="_blank" rel="noopener noreferrer">vincent qu's</a> portfolio</p>
+      <p><a href="https://github.com/VincentQu888" target="_blank" rel="noopener noreferrer">vincent qu&apos;s</a> portfolio</p>
       <p>this portfolio is gonna be fire trust</p>
       <p>wip</p>
       <br/>
@@ -11,8 +9,21 @@ export default function Home() {
       <p>cs @ uoft + schulich leader scholarship</p>
       <p>incoming @ shopify</p>
       <p>learning about ml</p>
+      <br/>
       <div data-webring="ca" data-member="vincent"></div>
       <script src="https://webring.ca/embed.js" defer></script>
+      <br/>
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '8px' }} className="w-full">
+        <a href='https://uoftwebring.com/redirect?nav=prev&id=41'>←</a>
+        <a href='https://uoftwebring.com' target='_blank'>
+        <img
+          src='https://uoftwebring.com/ring_logo.svg'
+          alt='UofT Webring'
+          style={{width: '24px', height: 'auto'}}
+        />
+        </a>
+        <a href='https://uoftwebring.com/redirect?nav=next&id=41'>→</a>
+      </div>
     </div>
   );
 }
