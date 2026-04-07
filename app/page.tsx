7abe4fd866f2d0f0e12e60dc1bc630d3ebb051da
Nei -_- <VincentQu888@gmail.com>
2026-04-07 03:29:21 -0400

    added webring

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,17 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
+    <div className="p-8">
       <p>vincent qu's portfolio</p>
       <p>this portfolio is gonna be fire trust</p>
       <p>wip</p>
-    </>
+      <br/>
+      <p>about:</p>
+      <p>cs @ uoft + schulich leader scholarship</p>
+      <p>incoming @ shopify</p>
+      <p>learning about ml</p>
+      <div data-webring="ca" data-member="vincent"></div>
+      <script src="https://webring.ca/embed.js" defer></script>
+    </div>
   );
 }
